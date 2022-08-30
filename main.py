f5874216f3617207935726120ddca394dc5ae637
Christian D'Errico <christian.derrico@studio.unibo.it>
2022-08-30 05:39:46 +0200

    neural net ready

--- main.py
+++ main.py
@@ -3,14 +3,15 @@
 # Press Maiusc+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from pmlb import fetch_data
+import pandas as pd
 from neural_net_functions import build_model
 
 def import_dataframe(dataframes):
     dictionary = {}
 
     for df_name in dataframes:
-        df = fetch_data(df_name)
+        df = pd.read_csv(".\datasets_files\\" + df_name, index_col=[0],  sep=',', na_values=[''])
+        print(df.describe().transpose())
         dictionary[df_name] = df
 
     return dictionary
@@ -23,7 +24,7 @@ if __name__ == '__main__':
 
     dict = import_dataframe(dataframes)
     datasets = build_model(dict['auto'], 'auto')
-    print(len(dict['auto'].columns))
-    print(datasets['auto'][0].weights)
+    #print(len(dict['auto'].columns))
+    #print(datasets['auto'][0].weights)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
