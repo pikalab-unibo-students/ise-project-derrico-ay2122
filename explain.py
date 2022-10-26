2944a4cd33e3015ebeed86ec23264835c662e82e
Christian D'Errico <christian.derrico@studio.unibo.it>
2022-10-26 15:15:45 +0200

    explaining functions are completed

--- explain.py
+++ explain.py
@@ -1,8 +1,16 @@
+import math
 import time
 
-from encoding_functions_CPLEX import freeze_input_and_output_CPLEX, explain_CPLEX, delete_hypos_and_output_constraint, \
-    get_weights_and_bias, read_categorical_indexes, define_formula_CPLEX
-from encoding_functions_SMT import define_formula_SMT, explain_SMT, freeze_input_and_output_SMT, copy_solver
+import numpy as np
+from matplotlib import image as mpimg
+import matplotlib.cm as mpcm
+
+from encoding_functions_CPLEX import freeze_input_and_output_CPLEX, minimal_expl_CPLEX, \
+    delete_hypos_and_output_constraint, \
+    get_weights_and_bias, read_categorical_indexes, define_formula_CPLEX, smallest_expl_CPLEX
+from encoding_functions_SMT import define_formula_SMT, minimal_expl_SMT, freeze_input_and_output_SMT, copy_solver, \
+    smallest_expl_SMT
+
 
 def encoding_model(model, df_name, solver):
     assert solver in ["smt", "cplex"]
@@ -19,23 +27,72 @@ def encoding_model(model, df_name, solver):
     return pb, inputs
 
 
-def explaining_procedures(pb, solver_name, inputs, outputs):
-    rhypos = []
+def explaining_procedures(pb, expl_type, solver_name, pattern_number, df_name, inputs, outputs):
+    explanation = []
 
     if solver_name == "cplex":
         hypos = freeze_input_and_output_CPLEX(pb, inputs, outputs)
 
         start_time = time.time()
-        rhypos = explain_CPLEX(pb, hypos)
+        explanation = minimal_expl_CPLEX(pb, hypos) if expl_type == "minimal" else smallest_expl_CPLEX(pb, hypos)
         end_time = time.time() - start_time
 
         delete_hypos_and_output_constraint(pb)
     elif solver_name == "smt":
+
         s = copy_solver(pb)
         hypos = freeze_input_and_output_SMT(s, inputs, outputs)
+
         start_time = time.time()
-        rhypos = explain_SMT(s, hypos)
+        explanation = minimal_expl_SMT(s, hypos) if expl_type == "minimal" else smallest_expl_SMT(s, hypos)
         end_time = time.time() - start_time
 
-    print("LEN RHYPOS: ", len(rhypos))
-    return len(rhypos), end_time
+    if df_name == "mnist":
+        input_values = inputs[1]
+        expl = [v[0] for v in explanation]
+        save_images(input_values, expl, pattern_number)
+
+    print(len(explanation))
+
+    return len(explanation), end_time
+
+
+def save_images(sample, expl, pattern_id):
+
+        # image size
+        sz = int(math.sqrt(len(sample)))
+
+        light_blue_rgba = tuple([0, 255, 255, 230.0])
+        white_rgba = tuple([255, 255, 255, 255.0])
+        red_rgba = tuple([186, 6, 6, 255.0])
+        black_rgba = tuple([0, 0, 0, 255.0])
+
+        # original image
+        pixels1, pixels2 = [], []  # this will contain an array of masked pixels
+        for i in range(sz):
+            row1, row2 = [], []
+            for j, v in enumerate(sample[(i * sz):(i + 1) * sz]):
+                id_pixel = i * sz + j
+
+                if v == 1:
+                    if id_pixel in expl:
+                        row1.append(light_blue_rgba)
+                    else:
+                        row1.append(white_rgba)
+
+                    row2.append(white_rgba)
+                else:
+                    if id_pixel in expl:
+                        row1.append(red_rgba)
+                    else:
+                        row1.append(black_rgba)
+
+                    row2.append(black_rgba)
+
+            pixels1.append(row1)
+            pixels2.append(row2)
+
+        pixels1 = np.asarray(pixels1, dtype=np.uint8)
+        pixels2 = np.asarray(pixels2, dtype=np.uint8)
+        mpimg.imsave('.\\images\\sample{0}-patch.png'.format(pattern_id), pixels1, cmap=mpcm.gray, dpi=5)
+        mpimg.imsave('.\\images\\sample{0}-orig.png'.format(pattern_id), pixels2, cmap=mpcm.gray, dpi=5)
