9024214db848f4fdb8d6c409f0045493f2cde8d8
Christian D'Errico <christian.derrico@studio.unibo.it>
2022-10-26 15:26:20 +0200

    neural net functions fixed and completed

--- neural_net_functions.py
+++ neural_net_functions.py
@@ -7,54 +7,85 @@ from keras import Sequential
 from keras.layers import Dense
 from keras.models import clone_model
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 
-import seaborn as sns
+
+def mnist_preprocessing(df):
+    le_target = LabelEncoder()
+    df.loc[:, "target"] = le_target.fit_transform(df.loc[:, "target"])
+    max_value = 255
+
+    target, df = df['target'], df.drop(columns='target')
+
+    for j in range(df.shape[0]):
+        for i in range(df.shape[1]):
+            v = df.iloc[j, i]
+            diff_1 = max_value - v
+            df.iloc[j, i] = 0 if diff_1 > int(max_value / 2) else 1
+
+    categorical_ids = [v for v in range(len(df.columns))]
+    print_categorical_indexes(categorical_ids, "mnist")
+
+    df['target'] = target
+
+    return df
+
 
 def dataset_preprocessing(df, df_name=None):
 
-    df_new = df.copy()
-    categorical_features = [column for column in df.columns.values if "_categorical" in column or "_boolean" in column]
-    categorical_names = {}
+    if df_name == "mnist":
+        return mnist_preprocessing(df)
+    else:
+        df_new = df.copy()
+        categorical_features = [column for column in df.columns.values if "_categorical" in column]
+        print(categorical_features)
+        categorical_names = {}
 
-    there_are_categorical_features = categorical_features != []
+        there_are_categorical_features = categorical_features != []
 
-    if there_are_categorical_features:
-        for feature in categorical_features:
-            le = LabelEncoder()
-            le.fit(df.loc[:, feature])
-            categorical_names[feature] = le.classes_
-            df_new.loc[:, feature] = le.transform(df.loc[:, feature])
-
-        temp = pd.get_dummies(df_new.loc[:, categorical_features], columns=categorical_features)
-        df_new = df_new.loc[:, [v for v in df.columns if v not in categorical_features]].join(temp)
-
-    # target as categorical
-    le_target = LabelEncoder()
-    df_new.loc[:, "target"] = le_target.fit_transform(df.loc[:, "target"])
-
-    if df_name is not None:
-        ite = filter(lambda x: [v for v in categorical_features if v in x], df_new.columns)
-        ids = [df_new.drop(columns="target").columns.get_loc(c) for c in list(ite)]
         if there_are_categorical_features:
-            print_categorical_indexes(ids, df_name)
+            for feature in categorical_features:
+                le = LabelEncoder()
+                le.fit(df.loc[:, feature])
+                categorical_names[feature] = le.classes_
+                df_new.loc[:, feature] = le.transform(df.loc[:, feature])
 
-    return df_new
+            temp = pd.get_dummies(df_new.loc[:, categorical_features], columns=categorical_features)
+            df_new = df_new.loc[:, [v for v in df.columns if v not in categorical_features]].join(temp)
+
+        # target as categorical
+        le_target = LabelEncoder()
+        df_new.loc[:, "target"] = le_target.fit_transform(df.loc[:, "target"])
+
+        for column in df.columns.values:
+            if "_boolean" in column:
+                categorical_features.append(column)
+
+        if df_name is not None:
+            ite = filter(lambda x: [v for v in categorical_features if v in x], df_new.columns)
+            ids = [df_new.drop(columns="target").columns.get_loc(c) for c in list(ite)]
+            if there_are_categorical_features:
+                print_categorical_indexes(ids, df_name)
+
+        return df_new
+
 
 def print_categorical_indexes(ids, df_name):
-    with open(".\datasets_boolean_index\\" + df_name + "\\" + df_name + '_categorical_indexes.txt', 'w') as f:
+    with open(".\datasets_categorical_index\\" + df_name + "\\" + df_name + '_categorical_indexes.txt', 'w') as f:
         for id in ids:
             f.write(str(id) + "\n")
+
 
 def create_datasets(df):
 
     columns = [c for c in df.columns if c != 'target']
     x, y = df.loc[:, columns], df.loc[:, 'target']
+    print("Colonne: ", x.columns)
     x = StandardScaler().fit_transform(x)
 
     return x, y
+
 
 def plot_accuracy(history):
     plotter = plots.HistoryPlotter(smoothing_std=0)
@@ -69,11 +100,13 @@ def plot_accuracy(history):
     plt.ylabel('Loss')
     plt.show()
 
+
 def model_compiling(model):
     model.compile(loss='sparse_categorical_crossentropy',
                   run_eagerly=True,
                   optimizer="adam",
                   metrics=['accuracy'])
+
 
 def get_model(params):
 
@@ -82,8 +115,7 @@ def get_model(params):
     classes_number = params[2]
 
     model = Sequential([
-        Dense(nodes, activation='relu', name="firstlayer",
-              input_shape=[input_shape], bias_initializer=tf.initializers.Constant(0.1)),
+        Dense(nodes, activation='relu', name="firstlayer", input_dim=input_shape),
         Dense(classes_number, name="secondlayer", activation='softmax')
     ])
 
@@ -91,10 +123,11 @@ def get_model(params):
 
     return model
 
+
 def cross_fold(inputs, targets, model):
 
     # Define the K-fold Cross Validator
-    stratifiedKFold = StratifiedKFold(n_splits=5, shuffle=True)
+    stratifiedKFold = StratifiedKFold(n_splits=3, shuffle=True)
     acc_per_fold = []
     loss_per_fold = []
 
@@ -108,11 +141,14 @@ def cross_fold(inputs, targets, model):
         crossed_model = clone_model(model)
         model_compiling(crossed_model)
 
+        print(train.shape)
+        print(test.shape)
+
         # Generate a print
         print('------------------------------------------------------------------------')
         print(f'Training for fold {fold_no} ...')
 
-        crossed_model.fit(inputs, targets, batch_size=4, epochs=50, shuffle=True, callbacks=[early_stop])
+        crossed_model.fit(inputs[train], targets[train], batch_size=4, epochs=180, shuffle=True, callbacks=[early_stop])
 
         # Generate generalization metrics
         scores = crossed_model.evaluate(inputs[test], targets[test], verbose=0)
@@ -127,12 +163,15 @@ def cross_fold(inputs, targets, model):
     print("Average accuracy: ", np.mean(acc_per_fold))
     print("Average loss: ", np.mean(loss_per_fold))
 
+
 def fit_model(df, parameters, df_name):
 
     x, y = create_datasets(df)
 
     model = get_model(parameters)
-    cross_fold(x, y, model)
+    #cross_fold(x, y, model)
+    model_compiling(model)
+    model.fit(x, y, batch_size=4, epochs=100, shuffle=True)
 
     # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, stratify=y)
     #
@@ -149,6 +188,7 @@ def fit_model(df, parameters, df_name):
     # plt.show()
 
     return model
+
 
 #Procedura di creazione del modello
 def build_model(df, hidden_layer_nodes, df_name):
