67d97ae954aaf52011893e9e49be01ab926794e7
Christian D'Errico <christian.derrico@studio.unibo.it>
2022-11-12 14:38:54 +0100

    encoding utils are ready

--- encoding_utils_functions.py
+++ encoding_utils_functions.py
@@ -17,7 +17,7 @@ def set_type(id, categorical_ids, n_layer):
 def generate_variables(layer, categorical_ids, n_vars):
     return ['X_{0}_layer_{1}_type_{2}'.format(i, layer, set_type(i, categorical_ids, layer)) for i in range(n_vars)]
 
-def get_variables_names(id, n_of_layer):
+def get_support_variables_names(id, n_of_layer):
     suffix = '_{0}_layer_{1}_type_C'.format(id, n_of_layer+1)
     return ["X" + suffix, "S" + suffix] if n_of_layer == 0 else ["X" + suffix]
 
